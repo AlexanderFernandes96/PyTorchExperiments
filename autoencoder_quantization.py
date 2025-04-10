0e7d9efbb66f900af09d28731910ce23bfbcb230
AlexanderFernandes96 <alex9fernandes@gmail.com>
2025-04-09 21:48:46 -0400

    reduce everything to fully connected layers

--- autoencoder_quantization.py
+++ autoencoder_quantization.py
@@ -876,8 +876,8 @@ if __name__ == "__main__":
                   'train_val_split': 0.8,  # after the train/test split, split train data into train/val data
                   'lr': 10**(-1*np.random.uniform(2, 5)), # optimizer learning rate
                   # 'momentum': 0.9, # optimizer momentum for SGD
-                  'batch_size': 2**np.random.randint(5, 11), # batch training size
-                  'epochs': 5,  # total training duration
+                  'batch_size': 2**np.random.randint(6, 11), # batch training size
+                  'epochs': 500,  # total training duration
                   'snr_dB': -5, # transmit power to receive noise power
                   'epoch_val': 100, # validate early stop every epoch number
                   'epoch_echo': True, # flag to display epoch print losses
